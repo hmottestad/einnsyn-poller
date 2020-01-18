c6d66dcc313a5ad36ff5c1b36180cc38bc2d7aec
hmottestad <hmottestad@gmail.com>
2020-01-18 16:39:09 +0100

    Update runCurl.sh

--- runCurl.sh
+++ runCurl.sh
@@ -1,2 +1,23 @@
 curl -XPOST -H "Content-type: application/json" -d '{"size":50,"aggregations":{"contentTypes":"type","virksomheter":"arkivskaperTransitive"},"appliedFilters":[{"fieldName":"type","fieldValue":["JournalpostForMøte"],"type":"notQueryFilter"}],"sort":{}}' 'https://einnsyn.no/api/result'
+sleep 10
+curl -XPOST -H "Content-type: application/json" -d '{"size":50,"aggregations":{"contentTypes":"type","virksomheter":"arkivskaperTransitive"},"appliedFilters":[{"fieldName":"type","fieldValue":["JournalpostForMøte"],"type":"notQueryFilter"}],"sort":{},"offset":50}' 'https://einnsyn.no/api/result'
+sleep 10
+curl -XPOST -H "Content-type: application/json" -d '{"size":50,"aggregations":{"contentTypes":"type","virksomheter":"arkivskaperTransitive"},"appliedFilters":[{"fieldName":"type","fieldValue":["JournalpostForMøte"],"type":"notQueryFilter"}],"sort":{},"offset":100}' 'https://einnsyn.no/api/result'
+sleep 10
+
+
+curl -XPOST -H "Content-type: application/json" -d '{"size":50,"aggregations":{"contentTypes":"type","virksomheter":"arkivskaperTransitive"},"appliedFilters":[{"fieldName":"type","fieldValue":["JournalpostForMøte"],"type":"notQueryFilter"}],"sort":{"fieldName":"standardDato","order":"DESC"}}' 'https://einnsyn.no/api/result'
+sleep 10
+curl -XPOST -H "Content-type: application/json" -d '{"size":50,"aggregations":{"contentTypes":"type","virksomheter":"arkivskaperTransitive"},"appliedFilters":[{"fieldName":"type","fieldValue":["JournalpostForMøte"],"type":"notQueryFilter"}],"sort":{"fieldName":"standardDato","order":"DESC"},"offset":50}' 'https://einnsyn.no/api/result'
+sleep 10
+curl -XPOST -H "Content-type: application/json" -d '{"size":50,"aggregations":{"contentTypes":"type","virksomheter":"arkivskaperTransitive"},"appliedFilters":[{"fieldName":"type","fieldValue":["JournalpostForMøte"],"type":"notQueryFilter"}],"sort":{"fieldName":"standardDato","order":"DESC"},"offset":100}' 'https://einnsyn.no/api/result'
+sleep 10
+
+curl -XPOST -H "Content-type: application/json" -d '{"size":50,"aggregations":{"contentTypes":"type","virksomheter":"arkivskaperTransitive"},"appliedFilters":[{"fieldName":"type","fieldValue":["JournalpostForMøte"],"type":"notQueryFilter"}],"sort":{"fieldName":"standardDato","order":"ASC"}}' 'https://einnsyn.no/api/result'
+sleep 10
+curl -XPOST -H "Content-type: application/json" -d '{"size":50,"aggregations":{"contentTypes":"type","virksomheter":"arkivskaperTransitive"},"appliedFilters":[{"fieldName":"type","fieldValue":["JournalpostForMøte"],"type":"notQueryFilter"}],"sort":{"fieldName":"standardDato","order":"ASC"},"offset":50}' 'https://einnsyn.no/api/result'
+sleep 10
+curl -XPOST -H "Content-type: application/json" -d '{"size":50,"aggregations":{"contentTypes":"type","virksomheter":"arkivskaperTransitive"},"appliedFilters":[{"fieldName":"type","fieldValue":["JournalpostForMøte"],"type":"notQueryFilter"}],"sort":{"fieldName":"standardDato","order":"ASC"},"offset":100}' 'https://einnsyn.no/api/result'
+
+
 
