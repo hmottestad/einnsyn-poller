291c67b6f61922e715b074aa86ce3766b9ab7c06
hmottestad <hmottestad@gmail.com>
2020-01-28 21:04:29 +0100

    Update runCurl.sh

--- runCurl.sh
+++ runCurl.sh
@@ -1,3 +1,5 @@
+curl -XPOST -H "Content-type: application/json" -d '{"size":50,"aggregations":{"contentTypes":"type","virksomheter":"arkivskaperTransitive"},"appliedFilters":[{"fieldName":"type","fieldValue":["JournalpostForMøte"],"type":"notQueryFilter"}]}' 'https://einnsyn.no/api/result'
+sleep 10
 curl -XPOST -H "Content-type: application/json" -d '{"size":50,"aggregations":{"contentTypes":"type","virksomheter":"arkivskaperTransitive"},"appliedFilters":[{"fieldName":"type","fieldValue":["JournalpostForMøte"],"type":"notQueryFilter"}],"sort":{}}' 'https://einnsyn.no/api/result'
 sleep 10
 curl -XPOST -H "Content-type: application/json" -d '{"size":50,"aggregations":{"contentTypes":"type","virksomheter":"arkivskaperTransitive"},"appliedFilters":[{"fieldName":"type","fieldValue":["JournalpostForMøte"],"type":"notQueryFilter"}],"sort":{},"offset":50}' 'https://einnsyn.no/api/result'
